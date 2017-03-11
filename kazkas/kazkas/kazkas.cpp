6488ab70dce746c1740f4dbcb82c0367bd09db48
JurateBaltrukenaite <lilythu@gmail.com>
2017-03-11 16:13:32 +0200

    Update kazkas.cpp
    galingumas ir kainuoja

--- kazkas/kazkas/kazkas.cpp
+++ kazkas/kazkas/kazkas.cpp
@@ -17,11 +17,11 @@ struct automobiliai {
 	int PagaminimoMetai;
 	string spalva;
 	string degalai;
-	int galia;
+	int galingumas;
 	float SanaudosMieste;
 	float SanaudosUzmiesty;
 	string Kebulas;
-	float Kaina;
+	float Kainuoja;
 };
 
 
