6b4ddb893c48d94fde7039b106d48d2e8a011fa4
PLM16014 <PLM16014@VDUIF-501-11>
2017-03-22 09:32:58 +0200

    paskaitos darbas

--- rodykles1/rodykles1/rodykles12.cpp
+++ rodykles1/rodykles1/rodykles12.cpp
@@ -10,38 +10,34 @@
 //int
 //main()
 //{
-//	cout << "Funkcijos adresas " << &asmuo << endl;
+//	cout << "Funkcijos adresas " << &asmuo << endl; // &zenklas prie bet kokio kinamojo rodo jo adresa
 //	cout << asmuo << endl;
 //	system("pause");
 //	return 0;
 //}
 
-//#include <iostream>
-////03_cpp.cpp
-//using namespace std;
-//int
-//main()
-//{
-//	int kintamasis = 24;
-//	// kinamajam priskiriama reiksme = 24 */ 
-//	int *rodykle;
-//	// deklaruojamas kintamasis rodykle  
-//	//  dar sakoma: "rodykle á .
-//	//.."  
-//	rodykle = &kintamasis;
-//	// rodykle atsimena kintamojo adresà 
-//	// rodykl÷ rodo á kintamojo adresà 
-//	cout <<
-//		"Kintamojo reikðm÷ "<< *rodykle << endl;
-//	// spausdiname rodykl÷s rodomo kintamojo adreso sau
-//	//goma reikðm÷
-//		// 
-//		//Ásid÷m÷kite, kad rodykl÷s reikðm÷ :
-//	//*p
-//	cout << rodykle << endl;
-//	system("pause");
-//	return 0;
-//}
+#include <iostream> //03_cpp.cpp
+using namespace std;
+int main()
+{
+	int kintamasis = 24;
+	// kinamajam priskiriama reiksme = 24 */ 
+ int *rodykle; // deklaruojamas kintamasis rodykle,  dar sakoma: "rodykle á .//.."  
+	rodykle = &kintamasis;
+	// rodykle atsimena kintamojo adresà 
+	// rodykl÷ rodo á kintamojo adresà 
+	cout <<
+		"Kintamojo reikðm÷ "<< *rodykle << endl; // atsausdina 24;
+	// spausdiname rodykl÷s rodomo kintamojo adreso sau
+	//goma reikðm÷
+		// 
+		//Ásid÷m÷kite, kad rodykl÷s reikðm÷ :
+	//*p
+	cout << rodykle << endl; // kai rodykle be *, ji rodo kintamojo adresa, bet ne jo reiksme
+
+	system("pause");
+	return 0;
+}
 
 //#include <iostream>
 //// 05_cpp.cpp
@@ -68,4 +64,28 @@
 //}
 
 
-//aritmetiniai veiksmai
+////aritmetiniai veiksmai
+//#include <iostream> // 06_cpp.cpp 
+//using namespace std;
+//int main()
+//{
+//	int a;
+//	// kintamas a  9 sveikasis sk. 
+//	int *p;
+//	// kintamasis p 9 rodykl÷ 
+//	a = 99;
+//	p = &a;
+//	/* rodykle p rodo  i kint. a adresà */
+//	//     Aritmetinis veiksmas 
+//	(*p)++;
+//	// padidinama rodykl÷s rodomareikðm÷ VIENETU
+//
+//	cout << "Rodykles *p rodomas turinys " << *p << endl;
+//	cout << "rodykles rodomas adresas" << p << endl;
+//	cout << "kintamojo a adesas" << &a << endl;
+//
+//	// spausdinama kintamojo a reiksm÷  
+//	// jokiu bûdu ne p* 
+//	system("pause");
+//	return 0;
+//}
